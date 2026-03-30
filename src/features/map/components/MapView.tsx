e2dddf3f082760ab2ad4b1fc04f43ace90654be6
randalubis <randa.lubis@gmail.com>
2026-03-30 09:33:40 +0700

    Update MapView.tsx

--- src/features/map/components/MapView.tsx
+++ src/features/map/components/MapView.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, Marker, Popup, useMap, CircleMarker } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useEffect } from 'react';
